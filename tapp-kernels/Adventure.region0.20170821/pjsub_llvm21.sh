c0f5c77bcfc192e97d7c2b9c376cf064142c9d44
Emil VATAI <emil.vatai@gmail.com>
2026-03-30 15:19:07 +0900

    Reduce popsize

--- tapp-kernels/Adventure.region0.20170821/pjsub_llvm21.sh
+++ tapp-kernels/Adventure.region0.20170821/pjsub_llvm21.sh
@@ -4,7 +4,7 @@
 #PJM -N ML4T-fs-adv-21
 #PJM -L rscgrp=small
 #PJM -L elapse=2:00:00
-#PJM -L node=301
+#PJM -L node=21
 #PJM --mpi "max-proc-per-node=1"
 # #PJM --llio localtmp-size=40Gi
 #PJM -j -S
@@ -18,4 +18,4 @@ export LD_PRELOAD=/usr/lib/FJSVtcs/ple/lib64/libpmix.so
 # source /home/apps/oss/llvm-v19.1.4/init.sh		  
 module load LLVM/llvmorg-21.1.0
 
-mpirun -n 1 python -u app.py --n-threads 2 --method FugakuEvoTADASHI --population-size 300 --max-gen 10
+mpirun -n 1 python -u app.py --n-threads 2 --method FugakuEvoTADASHI --population-size 20 --max-gen 20
