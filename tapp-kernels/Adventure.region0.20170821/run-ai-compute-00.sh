38805e51c0d0b8e9e66e5d1be3448bcbded788e7
Emil Vatai <vatai@ai-compute-00.r-ccs27.riken.jp>
2026-04-04 13:06:14 +0900

    Add run-ai-compute-00.sh

---
+++ tapp-kernels/Adventure.region0.20170821/run-ai-compute-00.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ARGS=(
+  --method FugakuEvoTADASHI 
+  --n-threads 1 
+  --population-size=20 
+  --max-gen=50
+)
+LOGFILE=$(mktemp  -p . "adventure0.$(date -I).XXXXX.log")
+python -u app.py "${ARGS[@]}" 2>&1 > "$LOGFILE"
+
